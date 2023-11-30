722dde64d719c22387f2b881fbef33061ce7f1b4
austencloud <68725581+austencloud@users.noreply.github.com>
2023-11-30 08:24:25 -0600

    fixed start end widget layout

--- widgets/graph_editor/attr_panel/attr_box_widgets/start_end_widget.py
+++ widgets/graph_editor/attr_panel/attr_box_widgets/start_end_widget.py
@@ -6,6 +6,7 @@ from PyQt6.QtWidgets import (
     QComboBox,
     QVBoxLayout,
     QSpacerItem,
+    QSizePolicy,
 )
 from PyQt6.QtGui import QIcon, QFont
 from PyQt6.QtCore import Qt
@@ -30,7 +31,7 @@ class StartEndWidget(QWidget):
         super().__init__(attr_box)
         self.layout = QHBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.setSpacing(0)
+        self.layout.setSpacing(5)
         self.setFixedWidth(int(attr_box.width()))
 
         self.pictograph = pictograph
@@ -62,7 +63,9 @@ class StartEndWidget(QWidget):
         self.swap_button.setStyleSheet(self.attr_box.get_button_style())
         swap_icon_path = ICON_DIR + SWAP_ICON
         self.swap_button.setIcon(QIcon(swap_icon_path))
-        self.swap_button.setFixedSize(int(combo_box_height/2), int(combo_box_height/2))
+        self.swap_button.setFixedSize(
+            int(combo_box_height / 2), int(combo_box_height / 2)
+        )
         self.swap_button.clicked.connect(self.swap_locations)
 
         self.end_label = QLabel("End:", self)
@@ -74,41 +77,36 @@ class StartEndWidget(QWidget):
         self.end_combo_box.setFixedSize(
             combo_box_height + arrow_extra_width, combo_box_height
         )
-        
-        self.label_sized_spacer = QSpacerItem(0, int(combo_box_height / 2))
-        self.spacer_item = QSpacerItem(int(combo_box_height / 4), int(combo_box_height))
 
-        self.swap_layout = QVBoxLayout()
-        self.swap_layout.addItem(self.label_sized_spacer)
-        self.swap_layout.addWidget(self.swap_button)
-        self.swap_layout_parent = QHBoxLayout()
-        self.swap_layout_parent.addLayout(self.swap_layout)
-        self.swap_layout_parent.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.spacer_item = QWidget()
+        self.spacer_item.setFixedHeight(int(combo_box_height / 2))
+        self.spacer_item.setFixedWidth(int(combo_box_height / 2/2))
 
         self.spacer_layout = QVBoxLayout()
-        self.spacer_layout.addItem(self.spacer_item)
+        self.spacer_layout.addWidget(self.spacer_item)
 
         self.start_layout = QVBoxLayout()
         self.start_layout.addWidget(self.start_label)
         self.start_layout.addWidget(self.start_combo_box)
 
-        self.arrow_layout = QVBoxLayout()
-        self.arrow_layout.addWidget(self.swap_button)
-        self.arrow_layout.addWidget(self.arrow_label)
+        self.arrow_button_layout = QVBoxLayout()
+        self.arrow_button_layout.addWidget(self.swap_button)
+        self.arrow_button_layout.addWidget(self.arrow_label)
 
         self.end_layout = QVBoxLayout()
         self.end_layout.addWidget(self.end_label)
         self.end_layout.addWidget(self.end_combo_box)
 
-        self.locations_layout = QHBoxLayout()
-        self.locations_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.locations_layout.addLayout(self.start_layout)
-        self.locations_layout.addLayout(self.spacer_layout)
-        self.locations_layout.addLayout(self.arrow_layout)
-        self.locations_layout.addLayout(self.spacer_layout)
-        self.locations_layout.addLayout(self.end_layout)
+        self.start_end_layout = QHBoxLayout()
+        # set the alignment of the start and end layouts
+        self.start_end_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.start_end_layout.addLayout(self.start_layout)
+        # self.start_end_layout.addLayout(self.spacer_layout)
+        self.start_end_layout.addLayout(self.arrow_button_layout)
+        # self.start_end_layout.addLayout(self.spacer_layout)
+        self.start_end_layout.addLayout(self.end_layout)
 
-        self.layout.addLayout(self.locations_layout)
+        self.layout.addLayout(self.start_end_layout)
 
     def swap_locations(self) -> None:
         start_index = self.start_combo_box.currentIndex()
