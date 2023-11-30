6399a300f93978334a4a56ca8c7c5cf40efc936b
austencloud <68725581+austencloud@users.noreply.github.com>
2023-11-29 22:17:10 -0600

    Refactor start and end widget layout

--- widgets/graph_editor/attr_panel/attr_box_widgets/start_end_widget.py
+++ widgets/graph_editor/attr_panel/attr_box_widgets/start_end_widget.py
@@ -8,7 +8,8 @@ from PyQt6.QtWidgets import (
     QSpacerItem,
     QSizePolicy,
 )
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QFont
+from PyQt6.QtCore import Qt
 from settings.string_constants import ICON_DIR
 from typing import TYPE_CHECKING, List
 
@@ -18,8 +19,8 @@ from utilities.TypeChecking.TypeChecking import Locations
 if TYPE_CHECKING:
     from widgets.graph_editor.attr_panel.attr_box import AttrBox
 
-from PyQt6.QtGui import QFont
 from settings.string_constants import ICON_DIR, SWAP_ICON
+from PyQt6.QtWidgets import QSizePolicy
 
 
 class StartEndWidget(QWidget):
@@ -66,30 +67,38 @@ class StartEndWidget(QWidget):
             combo_box_height + arrow_extra_width, combo_box_height
         )
 
-        self.spacer = QSpacerItem(
-            0, int(combo_box_height / 2), QSizePolicy.Policy.Expanding
-        )
+        self.label_sized_spacer = QSpacerItem(0, int(combo_box_height / 2))
 
-        self.swapLayout = QVBoxLayout()
-        self.swapLayout.addItem(self.spacer)
-        self.swapLayout.addWidget(self.swapButton)
+        self.swap_layout = QVBoxLayout()
+        self.swap_layout.addItem(self.label_sized_spacer)
+        self.swap_layout.addWidget(self.swapButton)
 
         self.startLayout = QVBoxLayout()
         self.startLayout.addWidget(self.startLabel)
         self.startLayout.addWidget(self.startComboBox)
 
         self.arrow_layout = QVBoxLayout()
-        self.arrow_layout.addItem(self.spacer)
+        self.arrow_layout.addItem(self.label_sized_spacer)
         self.arrow_layout.addWidget(self.arrow_label)
 
         self.endLayout = QVBoxLayout()
         self.endLayout.addWidget(self.endLabel)
         self.endLayout.addWidget(self.endComboBox)
 
-        self.layout.addLayout(self.swapLayout)
-        self.layout.addLayout(self.startLayout)
-        self.layout.addLayout(self.arrow_layout)
-        self.layout.addLayout(self.endLayout)
+        self.locations_layout = QHBoxLayout()
+        self.locations_layout.addLayout(self.startLayout)
+        self.locations_layout.addLayout(self.arrow_layout)
+        self.locations_layout.addLayout(self.endLayout)
+        self.locations_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.swap_layout_parent = QHBoxLayout()
+        self.swap_layout_parent.addLayout(self.swap_layout)
+        self.swap_layout_parent.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.swap_layout_parent.setStretch(0, 1)  # Set swap layout to 1/4 of the width
+        self.swap_layout_parent.setStretch(1, 3)  # Set other layout to 3/4 of the width
+
+        self.layout.addLayout(self.swap_layout_parent)
+        self.layout.addLayout(self.locations_layout)
 
     def swap_locations(self) -> None:
         start_index = self.startComboBox.currentIndex()
