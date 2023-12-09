7cd563ada7e2d12f3f3aa2834f972df8240d2bd0
Austen Cloud <austencloud@gmail.com>
2023-12-09 12:26:34 -0600

    Update SVG files with XML declaration and namespace

--- resources/images/arrows/pro/add_namespace.py
+++ resources/images/arrows/pro/add_namespace.py
@@ -1,13 +1,11 @@
+from lxml import etree
 import os
-import xml.etree.ElementTree as ET
-
 
 def format_d_attribute(path_element, line_length=80):
-    d_attr = path_element.get("d")
+    d_attr = path_element.get('d')
     if not d_attr:
         return
 
-    # Split the 'd' attribute into lines of specified length
     formatted_d = ""
     line = ""
     for part in d_attr.split():
@@ -18,40 +16,34 @@ def format_d_attribute(path_element, line_length=80):
             line = part + " "
     formatted_d += line.strip()
 
-    # Update the 'd' attribute with the formatted string
-    path_element.set("d", formatted_d)
-
+    path_element.set('d', formatted_d)
 
 def add_namespace_to_svg(file_path, namespace):
     try:
-        tree = ET.parse(file_path)
+        parser = etree.XMLParser(remove_blank_text=True)
+        tree = etree.parse(file_path, parser)
         root = tree.getroot()
 
-        # Define the namespace and update the root tag
-        ET.register_namespace("", namespace)
-        root.tag = "{" + namespace + "}" + root.tag
+        root.set("xmlns", namespace)
 
-        # Format 'd' attributes
-        for path_element in root.findall(".//{http://www.w3.org/2000/svg}path"):
+        for path_element in root.xpath('//svg:path', namespaces={"svg": "http://www.w3.org/2000/svg"}):
             format_d_attribute(path_element)
 
-        # Write the modified tree back to the file
-        tree.write(file_path, encoding="utf-8", xml_declaration=True)
-    except ET.ParseError:
-        print(f"Failed to parse {file_path}")
-
+        tree.write(file_path, pretty_print=True, xml_declaration=True, encoding='UTF-8')
+    except etree.XMLSyntaxError as e:
+        print(f"Failed to parse {file_path}: {e}")
 
 def process_directory(directory, namespace):
     for subdir, dirs, files in os.walk(directory):
         for file in files:
-            if file.lower().endswith(".svg"):
+            if file.lower().endswith('.svg'):
                 file_path = os.path.join(subdir, file)
                 print(f"Processing {file_path}")
                 add_namespace_to_svg(file_path, namespace)
 
-
 # Set your directory and namespace here
-directory = "resources/images/arrows/pro"
+directory = "resources/images/arrows"
 namespace = "http://www.w3.org/2000/svg"
 
 process_directory(directory, namespace)
+
