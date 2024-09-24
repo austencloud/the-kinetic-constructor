f11ccb8017bdd800cf787acd73869bddad7166e6
austencloud <68725581+austencloud@users.noreply.github.com>
2024-09-24 15:33:03 -0500

    Refactor initial position calculation in ArrowPlacementManager

--- placement_managers/arrow_placement_manager/components/arrow_initial_pos_calculator.py
+++ placement_managers/arrow_placement_manager/components/arrow_initial_pos_calculator.py
@@ -12,18 +12,18 @@ class ArrowInitialPosCalculator:
     def __init__(self, placement_manager: "ArrowPlacementManager") -> None:
         self.pictograph: "BasePictograph" = placement_manager.pictograph
 
-    def get_initial_pos(self, arrow: Arrow) -> QPointF:
+    def get_initial_coords(self, arrow: Arrow) -> QPointF:
         if arrow.motion.motion_type in [PRO, ANTI, FLOAT]:
-            return self._get_diamond_shift_pos(arrow)
+            return self._get_shift_coords(arrow)
         elif arrow.motion.motion_type in [STATIC, DASH]:
-            return self._get_diamond_static_dash_pos(arrow)
+            return self._get_static_coords(arrow)
 
-    def _get_diamond_shift_pos(self, arrow: Arrow) -> QPointF:
+    def _get_shift_coords(self, arrow: Arrow) -> QPointF:
         layer = self.pictograph.grid.grid_data.layer2_points_normal
         point_name = f"{arrow.loc}_{self.pictograph.main_widget.grid_mode}_layer2_point"
         return layer.points[point_name].coordinates
 
-    def _get_diamond_static_dash_pos(self, arrow: Arrow) -> QPointF:
+    def _get_static_coords(self, arrow: Arrow) -> QPointF:
         layer = self.pictograph.grid.grid_data.hand_points_normal
         point_name = f"{arrow.loc}_{self.pictograph.main_widget.grid_mode}_hand_point"
         return layer.points[point_name].coordinates
