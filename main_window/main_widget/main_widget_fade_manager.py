47cf9ac288b5e41ced7f0fbf000f107f269dc12c
austencloud <68725581+austencloud@users.noreply.github.com>
2024-12-27 23:35:11 -0600

    making tab switching much prettier

--- main_window/main_widget/main_widget_fade_manager.py
+++ main_window/main_widget/main_widget_fade_manager.py
@@ -7,6 +7,7 @@ from PyQt6.QtCore import (
     QAbstractAnimation,
     QEasingCurve,
     pyqtSlot,
+    QParallelAnimationGroup,
 )
 
 from main_window.main_widget.construct_tab.option_picker.option_picker import (
@@ -106,3 +107,103 @@ class MainWidgetFadeManager(QObject):
             effect = QGraphicsOpacityEffect(widget)
             widget.setGraphicsEffect(effect)
         return effect
+
+    def fade_both_stacks_in_parallel(
+        self,
+        right_stack: QStackedLayout,
+        right_new_index,
+        left_stack: QStackedLayout,
+        left_new_index,
+    ):
+        # 1) Gather old widgets
+        old_right_idx = right_stack.currentIndex()
+        old_left_idx = left_stack.currentIndex()
+
+        if old_right_idx == right_new_index and old_left_idx == left_new_index:
+            return  # Already on those pages, do nothing
+
+        old_right_widget = right_stack.widget(old_right_idx)
+        old_left_widget = left_stack.widget(old_left_idx)
+        new_right_widget = right_stack.widget(right_new_index)
+        new_left_widget = left_stack.widget(left_new_index)
+
+        if not old_right_widget or not old_left_widget:
+            return
+
+        # 2) Create parallel fade-out group
+        self.fade_out_group = QParallelAnimationGroup(self)  # Pass `self` as parent
+    
+        old_right_effect = self._ensure_opacity_effect(old_right_widget)
+        old_left_effect = self._ensure_opacity_effect(old_left_widget)
+
+        anim_out_right = QPropertyAnimation(old_right_effect, b"opacity", self)
+        anim_out_right.setDuration(self.duration)
+        anim_out_right.setStartValue(1.0)
+        anim_out_right.setEndValue(0.0)
+        anim_out_right.setEasingCurve(QEasingCurve.Type.InOutQuad)
+
+        anim_out_left = QPropertyAnimation(old_left_effect, b"opacity", self)
+        anim_out_left.setDuration(self.duration)
+        anim_out_left.setStartValue(1.0)
+        anim_out_left.setEndValue(0.0)
+        anim_out_left.setEasingCurve(QEasingCurve.Type.InOutQuad)
+
+        self.fade_out_group.addAnimation(anim_out_right)
+        self.fade_out_group.addAnimation(anim_out_left)
+
+        self.fade_out_group.finished.connect(
+            lambda: self._switch_and_fade_in_both(
+                right_stack,
+                right_new_index,
+                new_right_widget,
+                left_stack,
+                left_new_index,
+                new_left_widget,
+            )
+        )
+
+        self.fade_out_group.start()
+        self._is_animating = True  # Mark that we’re in a fade now
+
+    def _switch_and_fade_in_both(
+        self,
+        right_stack: QStackedLayout,
+        right_new_index,
+        new_right_widget,
+        left_stack: QStackedLayout,
+        left_new_index,
+        new_left_widget,
+    ):
+        # 3) Switch each stack’s index
+        right_stack.setCurrentIndex(right_new_index)
+        left_stack.setCurrentIndex(left_new_index)
+
+        # Reset old widgets’ opacity to 1 for next time
+        # (if you need them to show up fully when switched back)
+        # old_right_effect.setOpacity(1.0)
+        # old_left_effect.setOpacity(1.0)
+
+        # 4) Create parallel fade-in group
+        self.fade_in_group = QParallelAnimationGroup()
+
+        new_right_effect = self._ensure_opacity_effect(new_right_widget)
+        new_right_effect.setOpacity(0.0)
+        anim_in_right = QPropertyAnimation(new_right_effect, b"opacity", self)
+        anim_in_right.setDuration(self.duration)
+        anim_in_right.setStartValue(0.0)
+        anim_in_right.setEndValue(1.0)
+        anim_in_right.setEasingCurve(QEasingCurve.Type.InOutQuad)
+
+        new_left_effect = self._ensure_opacity_effect(new_left_widget)
+        new_left_effect.setOpacity(0.0)
+        anim_in_left = QPropertyAnimation(new_left_effect, b"opacity", self)
+        anim_in_left.setDuration(self.duration)
+        anim_in_left.setStartValue(0.0)
+        anim_in_left.setEndValue(1.0)
+        anim_in_left.setEasingCurve(QEasingCurve.Type.InOutQuad)
+
+        self.fade_in_group.addAnimation(anim_in_right)
+        self.fade_in_group.addAnimation(anim_in_left)
+
+        self.fade_in_group.finished.connect(self._on_fade_in_finished)
+        self.fade_in_group.start()
--- main_window/main_widget/main_widget_tabs.py
+++ main_window/main_widget/main_widget_tabs.py
@@ -13,22 +13,20 @@ class MainWidgetTabs:
         self.mw = main_widget
 
     def on_tab_changed(self, index: int) -> None:
-        tab_names = {
-            self.mw.construct_tab_index: "build",
-            self.mw.generate_tab_index: "generate",
-            self.mw.browse_tab_index: "browse",
-            self.mw.learn_tab_index: "learn",
-            self.mw.write_tab_index: "write",
-        }
-        if index in tab_names:
-            self.mw.settings_manager.global_settings.set_current_tab(tab_names[index])
+        if index == self.mw.learn_tab_index and self.mw.right_stack.currentIndex() == index:
+            return
 
-        self.mw.stack_fade_manager.fade_to_tab(self.mw.right_stack, index)
-        QApplication.processEvents()
         if index == self.mw.learn_tab_index:
-            self.mw.stack_fade_manager.fade_to_tab(self.mw.left_stack, 1)
+            left_new_index = 1
         else:
-            self.mw.stack_fade_manager.fade_to_tab(self.mw.left_stack, 0)
+            left_new_index = 0
+
+        self.mw.stack_fade_manager.fade_both_stacks_in_parallel(
+            right_stack=self.mw.right_stack,
+            right_new_index=index,
+            left_stack=self.mw.left_stack,
+            left_new_index=left_new_index,
+        )
 
     def update_tab_based_on_settings(self) -> None:
         """Switch to the tab indicated by saved settings."""
